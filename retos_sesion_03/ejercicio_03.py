fb4229739c4670558fcce0858795572923007376
Cammy Santander <alexmmy2215@gmail.com>
2025-05-09 19:57:01 -0400

    Tutorial de como lavar los platos detallado

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,35 @@
+print("TUTORIAL: CÓMO LAVAR LOS PLATOS\n")
+
+print("1. Reúne todos los materiales necesarios:")
+print("- Esponja suave (de doble cara: una suave y otra abrasiva).")
+print("- Jabón líquido para platos.")
+print("- Guantes de limpieza (opcional, recomendable si tienes piel sensible, como en mi caso).")
+print("- Virulana o Virulina (como la conozcas).")
+print("- Secador de platos o paño limpio de microfibra.")
+print("- Escurridor de platos.")
+
+print("\n2. Preparación:")
+print("- Retira restos de comida de los platos y ollas con una servilleta o espátula.")
+print("- Llena un bañador o fregadero con agua.")
+print("- Coloca el jabon líquido en la esponja (lo necesario).")
+
+print("\n3. Orden para lavar los platos:")
+print("  a. Vasos y tazas.")
+print("  b. Cubiertos.")
+print("  c. Platos.")
+print("  e. Bandejas y fuentes grandes.")
+print("  f. Ollas y sartenes.")
+
+print("\n4. Cómo lavar cada pieza:")
+print("- Moja la esponja con agua.")
+print("- Frota suavemente en círculos, o segun la estructura de la pieza.")
+print("- Enjuaga cada pieza con agua para eliminar toda la espuma del jabon liquido.")
+print("- Coloca los platos en el escurridor o sobre un paño limpio.")
+
+print("\n5. Secado:")
+print("- Deja secar al aire en el escurridor.")
+print("- O seca con un paño limpio de microfibra.")
+
+print("\n6. Para el final:")
+print("- Limpia el fregadero o bañador con agua y la esponja.")
+print("- Lava la esponja y déjala secar al aire o en el microondas para desinfectar.")
