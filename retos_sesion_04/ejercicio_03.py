d3c9f4822b003990c7be514bfac2be0bd83cf021
Cammy Santander <alexmmy2215@gmail.com>
2025-05-10 18:42:41 -0400

    Reglas de mi juego de mesa favorito UNO

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,35 @@
+# Reglas del juego UNO
+
+print("UNO")
+print("\nReglas del juego:")
+
+# Regla 1: Objetivo del juego
+print("\n1. Objetivo del juego:")
+print("El objetivo de UNO es ser el primer jugador en quedarse sin cartas.")
+print("Los jugadores deben ir colocando sus cartas de acuerdo con el color o el número de la carta en la pila.")
+print("Cuando un jugador tiene solo una carta, debe gritar 'UNO'.\n")
+
+# Regla 2: El mazo
+print("2. El mazo:")
+print("El mazo de UNO consta de 108 cartas, que incluyen números del 0 al 9 en cuatro colores: rojo, amarillo, verde y azul.")
+print("Hay cartas especiales:\n")
+print("a) +2: Al siguiente jugador se le adicionan dos cartas.")
+print("b) +4: Al siguiente jugador se le adicionan cuatro cartas.")
+print("c) Reversa: Cambia el sentido de juego (de izquierda a derecha o viceversa).")
+print("d) Salto o Bloqueo: El siguiente jugador pierde su turno.")
+print("e) Cambio de Color: El jugador elige el nuevo color a jugar.\n")
+
+# Regla 3: Cómo se juega
+print("3. Cómo se juega:")
+print("a) Se coloca una carta normal numerada cualquiera del mazo en el centro para que se forme la pila.")
+print("b) El jugador que comienza pone una carta en el centro que coincida en color o número con la carta en la pila.")
+print("c) Si un jugador no tiene cartas que coincidan, debe tomar una carta del mazo.")
+print("d) Si la carta tomada coincide, el jugador puede jugarla inmediatamente.")
+print("d) Si la carta tomada no coincide, el jugador puede decir paso y pasar su turno.")
+print("e) Tambien se pueden jugar con las cartas especiales segun las reglas de cada una.\n")
+
+# Regla 4: Final del juego
+print("4. Final del juego:")
+print("El jugador que grito 'UNO' debe deshacerse de su ultima carta y sera el ganador.")
+print("Si aun hay mas jugadores se continua de la misma manera usando el mazo del reverso si ya no tienen cartas de donde alzar.")
+print("El juego termina cuando solo queda un último jugador con cartas y sera el que pierda.\n")
